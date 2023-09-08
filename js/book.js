aa6c2e9bddf09f8aaa4078dfe2947519a6e4bfe4
Ezequielgr05 <gomezrossiezequiel@gmail.com>
2023-09-07 21:39:09 -0300

    terminado los libros

--- js/book.js
+++ js/book.js
@@ -1,47 +1,70 @@
-book = document.querySelectorAll(".book")
-sectionBook = document.querySelectorAll(".sectionBook")
-btnInfo = document.querySelectorAll(".btnInfo")
-btnLeer = document.querySelectorAll(".btnLeer")
-titleBook = document.querySelectorAll(".titleBook")
+book = document.querySelectorAll(".book");
+sectionBook = document.querySelectorAll(".sectionBook");
+btnInfo = document.querySelectorAll(".btnInfo");
+btnLeer = document.querySelectorAll(".btnLeer");
+titleBook = document.querySelectorAll(".titleBook");
+imgBook = document.querySelectorAll(".imgBook")
 
-book = Array.from(book)
-sectionBook = Array.from(sectionBook)
-btnInfo = Array.from(btnInfo)
-btnLeer = Array.from(btnLeer)
-titleBook = Array.from(titleBook)
+book = Array.from(book);
+sectionBook = Array.from(sectionBook);
+btnInfo = Array.from(btnInfo);
+btnLeer = Array.from(btnLeer);
+titleBook = Array.from(titleBook);
+imgBook = Array.from(imgBook)
 
 let hoverActivo = false;
+let bookActivo;
 
-book[0].addEventListener('mouseenter', () => {
-    sectionBook[0].classList.add("abierto")
-    sectionBook[0].classList.remove("cerrado")
-    setTimeout(() => {
-        titleBook[0].classList.add("titleBookT")
-        titleBook[0].classList.remove("titleBookN")
-    }, 500);
-    setTimeout(() => {
-        btnInfo[0].classList.add("btnBook")
-        btnInfo[0].classList.remove("btnBookNo")
-        btnInfo[0].innerHTML = "+ Informacion"
-        btnLeer[0].classList.add("btnBook")
-        btnLeer[0].classList.remove("btnBookNo")
-        btnLeer[0].innerHTML = "Leer"
-    }, 800);
-});
+for (let i in book) {
+    book[i].addEventListener("mouseenter", () => {
+        // Cuando se hace hover, actualizamos el valor de bookActivo
+        let bookActivo = i;
 
-book[0].addEventListener('mouseleave', () => {
-    sectionBook[0].classList.remove("abierto")
-    sectionBook[0].classList.add("cerrado")
-    setTimeout(() => {
-        titleBook[0].classList.remove("titleBookT")
-        titleBook[0].classList.add("titleBookN")
-    }, 500);
-    setTimeout(() => {
-        btnInfo[0].classList.remove("btnBook")
-        btnInfo[0].classList.add("btnBookNo")
-        btnInfo[0].innerHTML = " "
-        btnLeer[0].classList.remove("btnBook")
-        btnLeer[0].classList.add("btnBookNo")
-        btnLeer[0].innerHTML = " "
-    }, 100);
-});
+        // Verificar si el índice es válido
+        if (bookActivo >= 0 && bookActivo < sectionBook.length) {
+            // Lógica para mostrar/ocultar elementos aquí
+            sectionBook[bookActivo].classList.toggle("abierto");
+            sectionBook[bookActivo].classList.toggle("cerrado");
+            imgBook[bookActivo].classList.toggle("imgBookAbierto");
+
+            setTimeout(() => {
+                titleBook[bookActivo].classList.toggle("titleBookT");
+                titleBook[bookActivo].classList.toggle("titleBookN");
+            }, 500);
+
+            setTimeout(() => {
+                btnInfo[bookActivo].classList.toggle("btnBook");
+                btnInfo[bookActivo].classList.toggle("btnBookNo");
+                btnInfo[bookActivo].textContent = "+ Informacion";
+
+                btnLeer[bookActivo].classList.toggle("btnBook");
+                btnLeer[bookActivo].classList.toggle("btnBookNo");
+                btnLeer[bookActivo].textContent = "Leer";
+            }, 800);
+        }
+    });
+
+    book[i].addEventListener('mouseleave', () => {
+        // Verificar si el índice es válido
+        if (i >= 0 && i < sectionBook.length) {
+            // Lógica para restaurar elementos cuando se sale del hover
+            sectionBook[i].classList.toggle("abierto");
+            sectionBook[i].classList.toggle("cerrado");
+
+            imgBook[i].classList.toggle("imgBookAbierto")
+
+            btnInfo[i].classList.toggle("btnBook");
+            btnInfo[i].classList.toggle("btnBookNo");
+            btnInfo[i].textContent = " ";
+
+            btnLeer[i].classList.toggle("btnBook");
+            btnLeer[i].classList.toggle("btnBookNo");
+            btnLeer[i].textContent = " ";
+
+            setTimeout(() => {
+                titleBook[i].classList.toggle("titleBookT");
+                titleBook[i].classList.toggle("titleBookN");
+            }, 500);
+        }
+    });
+}
