bccce87dc47f5da2065660ddf4c8409931a0b2c5
adrielllll <adrielserafini@gmail.com>
2023-09-11 13:00:33 -0300

    todavia no funciona

--- js/lectorPDF.js
+++ js/lectorPDF.js
@@ -4,3 +4,24 @@ myState = {
     zoom: 1
 }
 
+pdfjsLib.getDocument('./pdfs/ManualdeGuitarra').then((pdf) => {
+    myState.pdf = pdf
+    render()
+})
+
+function render(){
+    myState.pdf.getPage(myState.currentPage).then((page) => {
+        canvas = document.querySelector("#pdf_renderer")
+        ctx = canvas.getContext('2d')
+
+        viewport = page.getViewport(myState.zoom)
+
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+    })
+}
+
+page.render({
+    canvaContext: ctx,
+    viewport: viewport
+})
