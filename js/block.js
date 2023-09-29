b4d574fe31662eec13053667c3e2e79b2bc7332c
julian71406 <julianpccc72@gmail.com>
2023-09-29 18:24:35 -0300

    bloqueo

--- js/block.js
+++ js/block.js
@@ -1,5 +1,14 @@
 const contenidoGuardado = document.documentElement.innerHTML;
 
+if (/.*Mobi|.*Android/i.test(navigator.userAgent)) {
+    if (window.orientation === 0 || window.orientation === 180) {
+        document.body.innerHTML = '';
+    }
+    else if (window.orientation === 90 || window.orientation === -90) {
+        document.documentElement.innerHTML = contenidoGuardado;
+    }
+}
+
 window.addEventListener("orientationchange", () => {
     if (/.*Mobi|.*Android/i.test(navigator.userAgent)) {
         if (window.orientation === 0 || window.orientation === 180) {
