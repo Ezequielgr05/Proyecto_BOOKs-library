38217e5b60b5aa4afe3c149b532457dd3fd8fc28
Ezequielgr05 <gomezrossiezequiel@gmail.com>
2023-10-03 16:25:09 -0300

    falta mejorar el bloqueador

--- js/block.js
+++ js/block.js
@@ -1,23 +1,35 @@
 const contenidoGuardado = document.documentElement.innerHTML;
 
 if (/.*Mobi|.*Android/i.test(navigator.userAgent)) {
-    if (window.orientation === 0 || window.orientation === 180) {
-        document.body.innerHTML = '';
+  if (window.orientation === 0 || window.orientation === 180) {
+    document.body.innerHTML = "";
+    advertencia = document.createElement("h1");
+    advertencia.textContent = "Para poder usar esta web gire el dispositivo";
+    advertencia.classList.add("advertencia");
+    document.body.appendChild(advertencia);
+  } else if (window.orientation === 90 || window.orientation === -90) {
+    advertencia = document.querySelector(".advertencia");
+    if (advertencia) {
+      advertencia.remove();
     }
-    else if (window.orientation === 90 || window.orientation === -90) {
-        document.documentElement.innerHTML = contenidoGuardado;
-    }
+    document.documentElement.innerHTML = contenidoGuardado;
+  }
 }
 
-
 window.addEventListener("orientationchange", () => {
-    if (/.*Mobi|.*Android/i.test(navigator.userAgent)) {
-        if (window.orientation === 0 || window.orientation === 180) {
-            document.body.innerHTML = '';
-        }
-        else if (window.orientation === 90 || window.orientation === -90) {
-            document.documentElement.innerHTML = contenidoGuardado;
-        }
+  if (/.*Mobi|.*Android/i.test(navigator.userAgent)) {
+    if (window.orientation === 0 || window.orientation === 180) {
+      document.body.innerHTML = "";
+      advertencia = document.createElement("h1");
+      advertencia.textContent = "Para poder usar esta web gire el dispositivo";
+      advertencia.classList.add("advertencia");
+      document.body.appendChild(advertencia);
+    } else if (window.orientation === 90 || window.orientation === -90) {
+      advertencia = document.querySelector(".advertencia");
+      if (advertencia) {
+        advertencia.remove();
+      }
+      document.documentElement.innerHTML = contenidoGuardado;
     }
+  }
 });
-
